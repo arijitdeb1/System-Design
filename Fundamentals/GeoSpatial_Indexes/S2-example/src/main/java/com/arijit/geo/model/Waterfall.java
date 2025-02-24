45bee57337388b555b95360281015b3327629c7d
Arijit Deb <arijitdeb1@gmail.com>
2025-02-25 00:10:03 +0530

    Google-S2 example

--- Fundamentals/GeoSpatial_Indexes/S2-example/src/main/java/com/arijit/geo/model/Waterfall.java
+++ Fundamentals/GeoSpatial_Indexes/S2-example/src/main/java/com/arijit/geo/model/Waterfall.java
@@ -1,4 +1,18 @@
 package com.arijit.geo.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Waterfall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double latitude;
+    private double longitude;
+    private long s2CellId;
+    @Transient
+    private double distance;
 }
