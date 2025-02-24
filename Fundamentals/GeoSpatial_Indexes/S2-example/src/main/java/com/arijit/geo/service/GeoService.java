fac884a95776eb22c807024f23016a506b46bad2
Arijit Deb <arijitdeb1@gmail.com>
2025-02-25 00:11:05 +0530

    Google-S2 example

--- Fundamentals/GeoSpatial_Indexes/S2-example/src/main/java/com/arijit/geo/service/GeoService.java
+++ Fundamentals/GeoSpatial_Indexes/S2-example/src/main/java/com/arijit/geo/service/GeoService.java
@@ -1,4 +1,16 @@
 package com.arijit.geo.service;
 
+import com.google.common.geometry.S2CellId;
+import com.google.common.geometry.S2LatLng;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GeoService {
+
+    public long getS2CellId(double latitude, double longitude, int level) {
+        S2LatLng latLng = S2LatLng.fromDegrees(latitude, longitude);
+        return S2CellId.fromLatLng(latLng).parent(level).id();
+    }
 }
